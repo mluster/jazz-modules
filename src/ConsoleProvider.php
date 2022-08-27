58f6bfbac8a5c83506d70612933a75111e8837d6
mluster <luster.michael@gmail.com>
2022-08-27 15:43:39 -0500

    [#40] installed MAKE:POLICY feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\PolicyMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'PolicyMake' => 'command.policy.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerPolicyMake(): void
+    {
+        $this->app->singleton('command.policy.make', static function ($app) {
+            return new PolicyMake($app['files']);
         });
     }
 }
