2e9fd47cb04865e775f39027ec43d811b806ac43
mluster <luster.michael@gmail.com>
2022-08-27 16:46:38 -0500

    [#43] installed MAKE:RESOURCE feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\ResourceMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'ResourceMake' => 'command.resource.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerResourceMake(): void
+    {
+        $this->app->singleton('command.resource.make', static function ($app) {
+            return new ResourceMake($app['files']);
         });
     }
 }
