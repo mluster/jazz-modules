4a8fca25ec4746435e99124447a2369d554dc884
mluster <luster.michael@gmail.com>
2022-08-27 13:28:58 -0500

    [#38] installed MAKE:NOTIFICATION feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\NotificationMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'NotificationMake' => 'command.notification.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerNotificationMake(): void
+    {
+        $this->app->singleton('command.notification.make', static function ($app) {
+            return new NotificationMake($app['files']);
         });
     }
 }
