3c7a271c0e50eb7648266a3d38c90f61a3bc8f27
mluster <luster.michael@gmail.com>
2022-08-27 11:15:29 -0500

    [#31] installed MAKE:FACTORY feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\FactoryMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'FactoryMake' => 'command.factory.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerFactoryMake(): void
+    {
+        $this->app->singleton('command.factory.make', static function ($app) {
+            return new FactoryMake($app['files']);
         });
     }
 }
