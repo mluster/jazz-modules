d1b7e91811811b5598c23e46edbf7e0eab553d19
mluster <luster.michael@gmail.com>
2022-08-27 10:50:21 -0500

    [#28] initial MAKE:CONTROLLER command, blocked by Issues: #37,#42,#55

---
+++ src/Console/ControllerMake.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jazz\Modules\Console;
+
+use Illuminate\Routing\Console\ControllerMakeCommand;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+
+class ControllerMake extends ControllerMakeCommand
+{
+    use TGenerator {
+        buildClass as myBuildClass;
+    }
+
+    protected function buildClass($name): string
+    {
+        $stub = $this->myBuildClass($name);
+
+        if ($this->option('parent')) {
+            $parent = $this->qualifyModel($this->option('parent'));
+            $stub = $this->replaceParentModels($stub, $this->confirmModel($parent));
+        }
+        if ($this->option('model')) {
+            $model = $this->qualifyModel($this->option('model'));
+            $stub = $this->replaceModels($stub, $this->confirmModel($model));
+        }
+
+        $controllerNamespace = $this->getNamespace($name);
+        $stub = str_replace('use ' . $controllerNamespace . "\Controller;\n", '', $stub);
+
+        return $stub;
+    }
+
+    protected function confirmModel(string $model): string
+    {
+        if (!class_exists($model)) {
+            // $question = class_basename($model) . ' Model does not exist. Do you want to generate it?';
+            $question = 'Model does not exist. Do you want to generate it?';
+            if ($this->confirm($question)) {
+                $modelName = str_replace('\\', '/', Str::after($model, 'Models\\'));
+                $moduleKey = Config::get('modules.key');
+
+                $args = [
+                    'name' => $modelName,
+                    '--' . $moduleKey => $this->option($moduleKey),
+                ];
+                $this->call('make:model', $args);
+            }
+        }
+
+        return $model;
+    }
+
+    protected function getStub(): string
+    {
+        $stub = $this->getStubNonApi();
+        if ($this->option('api')) {
+            if ($stub !== null && !$this->option('invokable')) {
+                $stub = str_replace('.stub', '.api.stub', $stub);
+            }
+            if ($stub === null) {
+                $stub = 'controller.api.stub';
+            }
+        }
+        $stub = ($stub ?? 'controller.plain.stub');
+
+        return $this->getStubFile($stub);
+    }
+
+    private function getStubNonApi(): ?string
+    {
+        $stub = null;
+
+        if ($this->option('parent')) {
+            $stub = 'controller.nested.stub';
+        } elseif ($this->option('model')) {
+            $stub = 'controller.model.stub';
+        } elseif ($this->option('invokable')) {
+            $stub = 'controller.invokable.stub';
+        } elseif ($this->option('resource')) {
+            $stub = 'controller.stub';
+        }
+
+        return $stub;
+    }
+}
