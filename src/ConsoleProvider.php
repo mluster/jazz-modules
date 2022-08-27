3ce4139adba147cf9afb15c07fc4ef083fe3359d
mluster <luster.michael@gmail.com>
2022-08-27 16:51:57 -0500

    [#42] installed MAKE:REQUEST feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\RequestMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'RequestMake' => 'command.request.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerRequestMake(): void
+    {
+        $this->app->singleton('command.request.make', static function ($app) {
+            return new RequestMake($app['files']);
         });
     }
 }
