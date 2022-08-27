2127949ca886696696deecb1c619ee6a776f46d4
mluster <luster.michael@gmail.com>
2022-08-27 12:40:16 -0500

    [#32] installed MAKE:JOB feature

--- src/ConsoleProvider.php
+++ src/ConsoleProvider.php
@@ -7,11 +7,13 @@ namespace Jazz\Modules;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Jazz\Modules\Console\ConsoleMake;
+use Jazz\Modules\Console\JobMake;
 
 class ConsoleProvider extends ServiceProvider implements DeferrableProvider
 {
     protected array $commands = [
         'ConsoleMake' => 'command.console.make',
+        'JobMake' => 'command.job.make',
     ];
 
 
@@ -35,6 +37,13 @@ class ConsoleProvider extends ServiceProvider implements DeferrableProvider
     {
         $this->app->singleton('command.console.make', static function ($app) {
             return new ConsoleMake($app['files']);
+        });
+    }
+
+    protected function registerJobMake(): void
+    {
+        $this->app->singleton('command.job.make', static function ($app) {
+            return new JobMake($app['files']);
         });
     }
 }
