f18ab3f17e50f18f5ea7ea4eafdda69b636a97ee
mluster <luster.michael@gmail.com>
2022-09-28 12:45:39 -0500

    [#89] fixed FactoryResolveTest failures

--- src/Database/Factory.php
+++ src/Database/Factory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jazz\Modules\Database;
 
 use Illuminate\Database\Eloquent\Factories\Factory as LaravelFactory;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
 
@@ -12,13 +13,36 @@ abstract class Factory extends LaravelFactory
 {
     public static function resolveFactory(string $model): string
     {
+        $ret = null;
         if (Str::startsWith($model, 'App\\')) {
             $model = Str::startsWith($model, 'App\\Models\\')
                 ? Str::after($model, 'App\\Models\\')
                 : Str::after($model, 'App\\');
             $ret = static::$namespace . $model;
         } else {
-            $ret = Str::replace('\\Models\\', '\\Database\\Factories\\', $model);
+            $found = false;
+            $contexts = Config::get('modules.contexts');
+            foreach ($contexts as $options) {
+                $meta = $options['_meta'];
+                if (Str::startsWith($model, $meta['namespace'])) {
+                    $found = true;
+                    $ret = Str::replace('\\Models\\', '\\' . $meta['factories']['namespace'], $model);
+
+                    if (!class_exists($ret . 'Factory')) {
+                        $path = Str::replace($meta['namespace'], $meta['path'] . '/', $ret);
+                        $path = Str::replace(
+                            $meta['factories']['namespace'],
+                            $meta['assets'] . '/' . $meta['factories']['path'] . '/',
+                            $path
+                        );
+                        require_once(App::basePath(Str::replace('\\', '/', $path)) . 'Factory.php');
+                    }
+                    break;
+                }
+            }
+            if (!$found) {
+                $ret = Str::replace('\\Models\\', '\\Database\\Factories\\', $model);
+            }
         }
         return $ret . 'Factory';
     }
@@ -29,7 +53,20 @@ abstract class Factory extends LaravelFactory
         if (Str::startsWith('Database\\Factories\\', $name)) {
             $model = 'App\\Models\\' . Str::after($name, 'Database\\Factories\\');
         } else {
-            $model = Str::replace('\\Database\\Factories\\', '\\Models\\', $name);
+            $found = false;
+            $contexts = Config::get('modules.contexts');
+            foreach ($contexts as $options) {
+                $meta = $options['_meta'];
+                if (Str::startsWith($name, $meta['namespace'])) {
+                    $found = true;
+                    $model = Str::replace($meta['factories']['namespace'], 'Models//', $name);
+                    break;
+                }
+            }
+
+            if (!$found) {
+                $model = Str::replace('\\Database\\Factories\\', '\\Models\\', $name);
+            }
         }
         return $model;
     }
---
+++ tests/Database/FactoryResolveTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JazzTest\Modules\Database;
+
+use Illuminate\Support\Str;
+use JazzTest\Modules\ATestCase;
+use Illuminate\Database\Eloquent\Factories\Factory as LaravelFactory;
+
+class FactoryResolveTest extends ATestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testRun(string $name, ?string $module, string $factory, string $model): void
+    {
+        $args = [
+            'name' => $name,
+            '--factory' => true,
+            '--no-interaction' => true,
+        ];
+        if ($module) {
+            $args['--module'] = $module;
+        }
+        $this->createArtisan('make:model', $args);
+        if (!$module) {
+            $path = 'database/factories/' . Str::replace('.', '/', $name) . 'Factory.php';
+            require_once($this->app->basePath($path));
+        }
+
+        $this->assertEquals($factory, LaravelFactory::resolveFactoryName($model));
+
+        $obj = new $factory();
+        $this->assertEquals($model, $obj->modelName());
+    }
+    public function provider(): array
+    {
+        return [
+            ['MyResolve', null, 'Database\\Factories\\MyResolveFactory', 'App\\Models\\MyResolve'],
+            ['My.Resolve', null, 'Database\\Factories\\My\\ResolveFactory', 'App\\Models\\My\\Resolve'],
+            ['MyResolve', 'Sandbox', 'Module\\Sandbox\\Factories\\MyResolveFactory', 'Module\\Sandbox\\Models\\MyResolve'],
+            ['My.Resolve', 'Sandbox', 'Module\\Sandbox\\Factories\\My\\ResolveFactory', 'Module\\Sandbox\\Models\\My\\Resolve'],
+        ];
+    }
+}
